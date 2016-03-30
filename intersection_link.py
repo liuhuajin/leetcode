f845b42dc7d831f0a1c6c4bf75f9ab858ee0a63f
liuhj <liuhuajinbenton@163.com>
2016-03-30 21:26:37 +0800

    ~

--- intersection_link.py
+++ intersection_link.py
@@ -5,20 +5,20 @@ class ListNode(object):
 		
 class Solution(object):
 	def getIntersectionNode(self, headA, headB):
-	lenA = lenB = 0 
-	head = headA
-	while head:
-		head = head.next
-		len += 1
-	head = headB
-	while head:
-		head = head.next
-		len += 1
-	head1, head2 = headA, headB if lenA > lenB else headB, headA
-	delta = lenA - lenB if lenA > lenB else lenB - lenA
-	while delta:
-		head1 = head1.next
-	while not head1.val == head2.val:
-		head1 = head1.next
-		head2 = head2.next
-	return head1
+		lenA = lenB = 0 
+		head = headA
+		while head:
+			head = head.next
+			len += 1
+		head = headB
+		while head:
+			head = head.next
+			len += 1
+		head1, head2 = (headA, headB) if lenA > lenB else (headB, headA)
+		delta = lenA - lenB if lenA > lenB else lenB - lenA
+		while delta:
+			head1 = head1.next
+		while not head1.val == head2.val:
+			head1 = head1.next
+			head2 = head2.next
+		return head1
